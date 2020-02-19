1e0077bb15e848d97869702202796118c31e1a35
tellsela <tellsela@gmail.com>
2020-02-19 16:38:07 -0500

    add anim folder and xml, gradient background folder and xml

--- android/app/src/main/java/com/example/wallet/MainActivity.java
+++ android/app/src/main/java/com/example/wallet/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.wallet;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
+  private TextView tv;
+  private ImageView iv;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+//    setContentView(R.layout.launch);
+//    tv = (TextView) findViewById(R.id.tv);
+//    iv = (ImageView) findViewById(R.id.iv);
+//    Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+//    tv.startAnimation(myanim);
+//    iv.startAnimation(myanim);
     GeneratedPluginRegistrant.registerWith(this);
-
   }
 }
